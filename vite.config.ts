d98cce26ecc5c98845fd8c2d7bf5ea51a1e8f0ec
李梦晓 <limengxiao@qianxin.com>
2022-03-04 17:13:40 +0800

    feat: element plus自动引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import styleImport from 'vite-plugin-style-import'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ViteComponents, { ElementPlusResolver } from 'vite-plugin-components'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
+    ViteComponents({
+      customComponentResolvers: [ElementPlusResolver()],
     }),
     styleImport({
       libs: [
